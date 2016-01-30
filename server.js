b888a4aa208aed20f70d68da3a692e3b05f4245a
Ting <tiadeng@gmail.com>
2016-01-30 01:53:55 -0500

    signup

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express'),
     server = express();
-
+server.use(express.static(__dirname+'/css'));
 server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
 });
 
+server.post('/signup', function(req, res){
+  console.log('You signed up!');
+  res.redirect('/');
+});
 server.listen(8080, function(){
 	console.log("Now listening on port 8080");
 });
